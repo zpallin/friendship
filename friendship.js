59683f407bb741880812757fe359ca205f8da614
zpallin <zpallin@gmail.com>
2017-01-11 00:19:41 -0800

    Tell operation actually passes cli to the next node
    what have i done

--- friendship.js
+++ friendship.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 'use strict';
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var http = require('http');
-var sa  = require('superagent');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const sa  = require('superagent');
+const app = express();
+const exec = require('child_process').exec;
 
 // app uses
 app.use(bodyParser.json());
 
 // locals
-var Friend = require('./friendship/friend.js').Friend;
-var LocalDB = require('./friendship/localdb.js');
-var Phonebook = require('./friendship/phonebook.js');
-var cli = require('./friendship/cli.js');
-var state = require('./friendship/state.js');
-var helpers = require('./friendship/helpers.js');
+const Friend = require('./friendship/friend.js').Friend;
+const LocalDB = require('./friendship/localdb.js');
+const Phonebook = require('./friendship/phonebook.js');
+const cli = require('./friendship/cli.js');
+const state = require('./friendship/state.js');
+const helpers = require('./friendship/helpers.js');
+
 ////////////////////////////////////////////////////////////////////////////////
 // "static"
 var DEFAULT_ADDRESS = state.defaults.address;
@@ -52,11 +54,8 @@ function main() {
     console.log("\nRun \"--help\" with each action to learn more");
   }
 
+  // how to greet other nodes, aka add them
   if (helpers.isDefined(args.greet)) {
-
-    /*
-     * entrypoint for running tell arguments
-     */
 
     // first, find out if the target should be translated to an address.
     var target_address = args.target_friend; // the default
@@ -82,6 +81,37 @@ function main() {
           console.log(res.body);
         }
       });
+  }
+
+  // tell action
+  if (helpers.isDefined(args.tell)) {
+     // first, find out if the target should be translated to an address.
+    var target_address = args.target_friend; // the default
+
+    // check if the node name is myself? assign addr accordingly
+    if (target_address === me.name) {
+      target_address = me.address;
+ 
+    // loop through phonebook friends and check
+    } else {
+      target_address = phonebook.addr_by_name_or_return(target_address);
+    }
+
+    var addr = helpers.addr_from_string(target_address);
+    var sendData = JSON.stringify(me.data);
+
+    var data = me.data;
+    data.to_do = args.to_do
+
+    sa.post(addr.host + ":" + addr.port + "/tell")
+      .send(data)
+      .end(function(err, res) {
+        if (err !== null) {
+          console.log(err);
+        } else {
+          console.log(res.body);
+        }
+      }); 
   }
 
   //if (args.hasOwnProperty("action") &&
@@ -128,6 +158,27 @@ function main() {
 
       console.log("I was just greeted by " + greetName + "!");
 
+    });
+
+    app.post('/tell', function(req, res) {
+      var data = me.data;
+      data.message = "Thanks for your task";
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+
+      if (phonebook.get_friend_by_name(req.body.name) && req.body.crowd === me.crowd) {
+        console.log("I am being told to... \"" + req.body.to_do + "\""); 
+        
+        exec(req.body.to_do, (error, stdout, stderr) => {
+          if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        });
+      }
     });
 
     // get addr object and push some data
